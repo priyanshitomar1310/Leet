342bc4b5895c4a4b1e83576bc639f7faf46dfddf
priyanshitomar1310 <priyanshitomar1506@gmail.com>
2025-08-08 20:15:30 +0530

    Time: 1084 ms (26.75%) | Memory: 0B (100.00%) - LeetSync

--- 577-employee-bonus/employee-bonus.sql
+++ 577-employee-bonus/employee-bonus.sql
@@ -1,4 +1,3 @@
 SELECT e.name, b.bonus
 FROM Employee e
-LEFT JOIN Bonus b ON e.empId = b.empId
-WHERE b.bonus < 1000 OR b.bonus IS NULL;
+LEFT JOIN Bonus b ON e.empId = b.empId WHERE b.bonus < 1000 OR b.bonus IS NULL;
